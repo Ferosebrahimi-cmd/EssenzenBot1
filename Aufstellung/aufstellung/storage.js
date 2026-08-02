f05721ddb71ff9210e0ccc792f3e925cee1d458b
Feros <ferosebrahimi@gmx.net>
2026-08-02 05:56:29 +0200

    Aufstellung Speicherung repariert

--- Aufstellung/aufstellung/storage.js
+++ Aufstellung/aufstellung/storage.js
@@ -1,28 +1,137 @@
 const fs = require("fs");
 const path = require("path");
 
-const file = path.join(__dirname, "aufstellung.json");
+
+const file =
+    path.join(
+        __dirname,
+        "aufstellung.json"
+    );
+
+
 
 function load() {
 
+
     if (!fs.existsSync(file)) {
 
-        return {};
+        return {
+            messageId: null,
+            channelId: null,
+            alle: [],
+            dabei: []
+        };
 
     }
 
-    return JSON.parse(fs.readFileSync(file, "utf8"));
+
+
+    try {
+
+
+        const data =
+            JSON.parse(
+                fs.readFileSync(
+                    file,
+                    "utf8"
+                )
+            );
+
+
+        return {
+
+            messageId:
+                data.messageId || null,
+
+
+            channelId:
+                data.channelId || null,
+
+
+            alle:
+                Array.isArray(data.alle)
+                ? data.alle
+                : [],
+
+
+            dabei:
+                Array.isArray(data.dabei)
+                ? data.dabei
+                : []
+
+        };
+
+
+    } catch(error) {
+
+
+        console.error(
+            "❌ Fehler beim Laden:",
+            error
+        );
+
+
+        return {
+            messageId: null,
+            channelId: null,
+            alle: [],
+            dabei: []
+        };
+
+
+    }
 
 }
+
+
+
 
 function save(data) {
 
+
     fs.writeFileSync(
+
         file,
-        JSON.stringify(data, null, 4)
+
+        JSON.stringify(
+
+            {
+
+                messageId:
+                    data.messageId,
+
+
+                channelId:
+                    data.channelId,
+
+
+                alle:
+                    data.alle,
+
+
+                dabei:
+                    data.dabei
+
+            },
+
+            null,
+
+            4
+
+        )
+
     );
 
+
+    console.log(
+        "💾 Aufstellung gespeichert"
+    );
+
+
 }
+
+
+
 
 module.exports = {
 
