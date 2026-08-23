cff38d8130d664fb99e64c20b96ece5d95d26959
Feros <ferosebrahimi@gmx.net>
2026-08-23 04:42:08 +0200

    Discord Netzwerk Test entfernt

--- index.js
+++ index.js
@@ -324,25 +324,7 @@ const loginTimeout = setTimeout(() => {
     );
 
 }, 30000);
-const https = require("https");
 
-https.get("https://discord.com/api/v10/gateway", (res) => {
-
-    console.log(
-        "🌐 Discord API erreichbar:",
-        res.statusCode
-    );
-
-    res.on("data", () => {});
-
-}).on("error", (error) => {
-
-    console.error(
-        "❌ Discord API nicht erreichbar:",
-        error.message
-    );
-
-});
 client.login(process.env.TOKEN)
 
     .then(() => {
